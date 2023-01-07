54ef84442196159b4cb0a53748f63718d0480194
MetRiko <18maximus8050@gmail.com>
2023-01-07 22:11:00 +0100

    Add Game singleton

---
+++ Game.gd
@@ -0,0 +1,8 @@
+extends Node
+
+onready var root = get_tree().root.get_node("Root")
+
+onready var slimes = root.get_node("SlimeTesting/Slimes")
+
+func add_particle(particle : Node):
+	slimes.add_child(particle)
